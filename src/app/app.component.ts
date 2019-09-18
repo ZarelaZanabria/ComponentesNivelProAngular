ab3fe7c1defa9f9b7cf74eaa81082b943cb44f44
zzanabria <zzanabria@ebizlatin.com>
2019-09-18 11:22:55 -0500

    Estilo por atributos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component,  ViewChildren,  AfterContentInit, QueryList, AfterViewInit, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChildren, AfterContentInit, QueryList, AfterViewInit, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
 import { SimpleAlertViewComponent } from './componentes/simple-alert-view/simple-alert-view.component';
 
 @Component({
@@ -8,64 +8,65 @@ import { SimpleAlertViewComponent } from './componentes/simple-alert-view/simple
 })
 export class AppComponent implements AfterContentInit, AfterViewInit {
 
-  
-  public isAddTimerVisible : boolean = false;
-  public time : number = 0;
-  public timers : Array<number> = [];
 
-  @ViewChildren(SimpleAlertViewComponent) alerts : QueryList<SimpleAlertViewComponent>;
-  @ViewChild ("timerInput") timeInput : ElementRef;
-  constructor (private  cRef : ChangeDetectorRef) {
-    this.timers = [4,16,185];
+  public isAddTimerVisible: boolean = false;
+  public time: number = 0;
+  public timers: Array<number> = [];
+
+  @ViewChildren(SimpleAlertViewComponent) alerts: QueryList<SimpleAlertViewComponent>;
+  @ViewChild("timerInput") timeInput: ElementRef;
+  constructor(private cRef: ChangeDetectorRef) {
+    this.timers = [4, 16, 185];
 
   }
 
 
   logCountdowEnd() {
-    console.log("The countdown has  finished");    
+    console.log("The countdown has  finished");
   }
   ngAfterViewInit() {
 
     console.log('TimerIMPUT', this.timeInput);
     //Vamos agregar un atributo
-    this.timeInput.nativeElement.setAttibute("placeholder","enter seconds" );
+    this.timeInput.nativeElement.setAttibute("placeholder", "enter seconds");
+    this.timeInput.nativeElement.classList.add ("time-in");
 
-    
-    this.alerts.forEach(item =>{
 
-      if(!item.title){
+    this.alerts.forEach(item => {
+
+      if (!item.title) {
         item.title = "Hi!";
         item.message = "Welcome "
       }
       item.showAlert();
       console.log(item);
-      
+
     });
     this.cRef.detectChanges();
   }
   ngAfterContentInit(): void {
-   
+
   }
 
-  public showAddTimer (){
+  public showAddTimer() {
     this.isAddTimerVisible = true;
-    
-  }
-
-  public hideAddTimer(){
-    this.isAddTimerVisible = false ;
-  }
-
-  public showEndTimerAlert(){
-  this.alerts.first.showAlert();
 
   }
 
- 
+  public hideAddTimer() {
+    this.isAddTimerVisible = false;
+  }
+
+  public showEndTimerAlert() {
+    this.alerts.first.showAlert();
+
+  }
+
+
 
   public submitAddTimer() {
     this.timers.push(this.time);
     this.hideAddTimer();
   }
- 
+
 }
